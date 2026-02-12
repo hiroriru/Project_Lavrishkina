889ae64953c94a20c809193e70887aeadf508e86
hiroriru <nastalavris6@gmail.com>
2026-02-12 23:32:50 +0300

    хх

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -1,4 +1,4 @@
-'''
+"""
 Средствами языка Python сформировать текстовый файл (.txt), содержащий
 последовательность из целых положительных и отрицательных чисел. Сформировать
 новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую
@@ -9,43 +9,69 @@
 Повторяющиеся элементы:
 Количество повторяющихся элементов:
 Элементы больше 5 увеличены в два раза:
-'''
-l = ["-12 3 57 -8 9 53 -2 23"]
+"""
+
+l = ["-12 3 57 -8 3 53 -2 23"]
 f1 = open("data_1.txt", "w")
 f1.writelines(l)
 f1.close()
 
-#иСХОДНЫЕ ДАННЫЕ
-f2 = open('data_2.txt', 'w')
-f2.write('Исходные данные: ')
-f2.write('\n')
-f2.writelines(l)
-f2.close()
+f2 = open("data_2.txt", "w")
 
-#КОЛИЧЕСТВО ЭЛЕМЕНТОВ
-f1 = open('data_1.txt')
+f2.write("Исходные данные:")
+f2.write("\n")
+f2.writelines(l)
+f2.write("\n")
+
+f1 = open("data_1.txt")
 k = f1.read()
 k = k.split()
 for i in range(len(k)):
     k[i] = int(k[i])
 f1.close()
 
-#ПРОИЗВЕДЕНИЕ ЭЛЕМЕНТОВ ТТ
-f1 = open('data_1.txt')
-k = f1.read()
-k = k.split()
-t = 1
-for i in range(len(k)):  #ЧТО-ТО НАДО ПОМЕНЯТЬ, НО ПОКА МНЕ ЛЕНЬ
-    t = t * int(i)
-print(t)
-f1.close()
+kolichestvo = len(k)
+f2.write("Количество элементов:\n")
+f2.write(str(kolichestvo) + "\n")
 
-#ПОВТОРЯЮЩИЕСЯ ЭЛЕМЕНТЫ 
-f1 = open('data_1.txt')
-k = f1.read()
-k = k.split()
-n = 0
+t = 1
 for i in range(len(k)):
-    if i==i:
-        n += 1
-f1.close()
+    t = t * k[i]
+
+f2.write("Произведение элементов:\n")
+f2.write(str(t) + "\n")
+
+povtor = []
+for i in range(len(k)):
+    skolko_raz = 0
+    for j in range(len(k)):
+        if k[i] == k[j]:
+            skolko_raz = skolko_raz + 1
+    if skolko_raz > 1:
+        if k[i] not in povtor:
+            povtor.append(k[i])
+
+f2.write("Повторяющиеся элементы:\n")
+if len(povtor) == 0:
+    f2.write("Нет\n")
+else:
+    for i in range(len(povtor)):
+        f2.write(str(povtor[i]) + " ")
+    f2.write("\n")
+
+f2.write("Количество повторяющихся элементов:\n")
+f2.write(str(len(povtor)) + "\n") 
+
+el = []
+for i in range(len(k)):
+    if k[i] > 5:
+        el.append(k[i] * 2)
+    else:
+        el.append(k[i])
+
+f2.write("Элементы больше 5 увеличены в два раза:\n")
+for i in range(len(el)):
+    f2.write(str(el[i]) + " ")
+f2.write("\n")
+
+f2.close()
